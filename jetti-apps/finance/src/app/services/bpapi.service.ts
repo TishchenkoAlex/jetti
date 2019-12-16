4db17349951356cacdc40df4c97ed1d9e337dee4
Dbez <Dbez@srv-bp-01.sm.sushi-master.ru>
2019-12-16 10:54:38 +0300

    Добавлен Document.CashRequest

---
+++ jetti-apps/finance/src/app/services/bpapi.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class BPApi {
+
+    // GetTasks(Username: string, CountOfCompleted: number): Observable<TaskComponent>{
+    //     const httpOptions = {headers: this.headers, withCredentials: true};
+    //     const url = this.baseurl + '/Query/' + this.token + '/GetUserTasksByMail?UserMail=' + Username + ' &CountOfCompleted=' + CountOfCompleted;
+    //     return this.http.get<TaskComponent>(url, httpOptions).pipe(retry(1), catchError(this.errorHandl));
+    //   }
+}
