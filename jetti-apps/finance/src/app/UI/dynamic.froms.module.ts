5746d3362dff4a86f0a57ffdc1a22e5f77aec3ec
Dbez <Dbez@srv-bp-01.sm.sushi-master.ru>
2020-01-21 18:02:35 +0300

    hist comp

--- jetti-apps/finance/src/app/UI/dynamic.froms.module.ts
+++ jetti-apps/finance/src/app/UI/dynamic.froms.module.ts
@@ -27,7 +27,7 @@ import { CatalogListComponent } from './../UI/Catalog/calatog-list.component';
 import { OperationFormComponent } from './Operation/operation.form.component';
 import { OperationListComponent } from './Operation/operation.list.component';
 import { DisableControlDirective } from '../common/directives/disabled-control';
-import { HistoryComponent } from '../common/History/common.history';
+import { HistoryComponent } from '../common/history/common.history';
 
 @NgModule({
   declarations: [
