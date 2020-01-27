a3e6a4236e6d9643acce79061c8fecada1a9ea51
gimpon <dbez@outlook.com>
2020-01-27 21:58:24 +0200

    doc.DocumentOperation.parent readOnly

--- jetti-api/server/models/Documents/Document.Operation.ts
+++ jetti-api/server/models/Documents/Document.Operation.ts
@@ -21,7 +21,7 @@ import { DocumentBase, JDocument, Props, Ref } from './../document';
   ]
 })
 export class DocumentOperation extends DocumentBase {
-  @Props({ type: 'Types.Document', hiddenInList: true, order: -1 })
+  @Props({ type: 'Types.Document', hiddenInList: true, order: -1, readOnly: true })
   parent: Ref = null;
 
   @Props({
