5be6070846fb40fd4505eec40b9a6da1de42b454
Aleksandr Tishchenko <at@MacBook-Pro-Aleksandr.local>
2020-02-05 00:59:42 +0200

    pool refactor

--- jetti-api/server/sql.pool.jetti.ts
+++ jetti-api/server/sql.pool.jetti.ts
@@ -2,9 +2,8 @@ import * as sql from 'mssql';
 import { sqlConfig } from './env/environment';
 
 export const JETTI_POOL = new sql.ConnectionPool(sqlConfig);
+JETTI_POOL.on('error', (err: sql.ConnectionError) => console.error(err.message));
+
 JETTI_POOL.connect()
   .then(() => console.log('connected', sqlConfig.database))
   .catch(err => console.log('connection error', err));
-
-
-JETTI_POOL.on('error', (err: sql.ConnectionError) => console.error(err));
