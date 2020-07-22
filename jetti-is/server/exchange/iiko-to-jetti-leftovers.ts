b555008a002b474ba37dcec022d70bc32c340f50
IDONTSUDO <idontsudo@gmail.com>
2020-07-22 19:40:46 +0300

    Ivan clarification

--- jetti-is/server/exchange/iiko-to-jetti-leftovers.ts
+++ jetti-is/server/exchange/iiko-to-jetti-leftovers.ts
@@ -26,108 +26,8 @@ import { dateReviverUTC } from '../fuctions/dateReviver';
 import { text } from 'body-parser';
 import { stdout } from 'process';
 import { countReset } from 'console';
-
-///////////////////////////////////////////////////////////
+import responceLEFTROVERS = require('./responce_leftovers.json');
 const syncStage = 'Document.Purshase';
-///////////////////////////////////////////////////////////
-interface IPurchaseLeftRover {
-	project: string;
-	id: string;
-	baseid: string;
-	type: string;
-	documentNumber: string;
-	date: Date;
-	comment: string;
-	conception: string;
-	status: number;
-	store: string;
-	revision: string;
-	dateModified: Date;
-	userModified: string;
-	supplier: string;
-	incomingDate: Date;
-	incomingDocumentNumber: Number;
-}
-///////////////////////////////////////////////////////////
-
-const transformLeftRover = (
-	syncParams: ISyncParams,
-	source: any,
-): IPurchaseLeftRover => {
-	console.log(source);
-	return {
-		id: source.id,
-		comment: source.comment,
-		conception: source.conception,
-		date: source.dateIncoming,
-		documentNumber: source.documentNumber,
-		supplier: source.supplier,
-		incomingDate: source.incomingDate,
-		incomingDocumentNumber: source.incomingDocumentNumber,
-		revision: source.revision,
-		status: source.status,
-		project: syncParams.project.id,
-		baseid: syncParams.source.id,
-		type: 'PurshaseDoc',
-		store: source.defaultStore,
-		// ? what is typeDOC
-		dateModified: source.dateModified,
-		userModified: source.userModified,
-	};
-};
-///////////////////////////////////////////////////////////
-
-const newPurchase = () => {
-	return {
-		id: uuidv1().toUpperCase(),
-		type: 'Document.Operation',
-		code: '',
-		description: '',
-		posted: 0,
-		deleted: 0,
-		doc: {
-			Group: 'E74FF926-C149-11E7-BD8F-43B2F3011722',
-			// todo if (souce.id === urkaina) FBDEBFA0-3634-11EA-A656-856C3785004E
-			Operation: '6A2D525E-C149-11E7-9418-D72026C63174',
-			// todo first date
-			Amount: null,
-			currency: '',
-			f1: '',
-			f2: '',
-			PayDate: '',
-			Department: '',
-			Storehouse: '',
-			Suplier: '',
-			Contract: '',
-			DocNumber: '',
-			DocDate: '',
-			DocReceived: '',
-			OriginalReceived: '',
-			ExchangeUser: '',
-			ExchangeModified: '',
-			ExchangeRevision: '',
-			Items: [],
-		},
-		parent: null,
-		isfolder: false,
-		company: '',
-		user: null,
-		info: null,
-	};
-};
-
-///////////////////////////////////////////////////////////
-async function syncLeftRoverSQL(
-	syncParams: ISyncParams,
-	iikoDoc: IPurchaseLeftRover,
-	sourceSQL: SQLClient,
-	destSQL: SQLClient,
-): Promise<any> {
-	try {
-	} catch (error) {
-		await saveLogProtocol(syncParams.syncid, 0, 1, syncStage, error.message);
-	}
-}
 
 export async function ImportLeftRoverToJetti(
 	syncParams: ISyncParams,
@@ -166,43 +66,97 @@ export async function ImportLeftRoverSQLToJetti(
 		}
 	}
 	const newSQL = `
-		SELECT
+	SELECT
 			sz.StoreID as SKU2,
 			sz.ProductID as SKU,
 			sz.Amount as Qty,
-			sz.Summa as Amount
-		FROM (
-		SELECT
-			cast(ttt.StoreID as nvarchar(50)) as StoreID,
+			sz.Summa as Amount  
+	FROM (
+	SELECT
+			cast(ttt.StoreID as nvarchar(50)) as StoreID,    
 			cast(ttt.ProductID as nvarchar(50)) as ProductID,
 			sum(ttt.Amount) as Amount,
 			sum(ttt.Summa) as Summa
-		FROM
-		(SELECT
+	FROM
+	(SELECT
 			tt.[from_account] as StoreID,
 			tt.[from_product] as ProductID,
 			coalesce(-tt.[from_amount],0) as Amount,
 			coalesce(-tt.[sum],0) as Summa
-		FROM [dbo].[AccountingTransaction] as tt
+	FROM [dbo].[AccountingTransaction] as tt
 			left join [dbo].[entity] en on (en.id=tt.[from_account])
-		WHERE tt.[date]< CONVERT(nvarchar(8),'@p1',112)  and en.type='Store'
-		and not tt.[from_product] is null
-		UNION ALL
-		SELECT
+	WHERE tt.[date]< CONVERT(nvarchar(8),@p1,112)  and en.type='Store'
+	and not tt.[from_product] is null
+	UNION ALL
+	SELECT
 			tt.[to_account],
 			tt.[to_product],
 			coalesce(tt.[to_amount],0),
 			coalesce(tt.[sum],0)
-		FROM [dbo].[AccountingTransaction] as tt
+	FROM [dbo].[AccountingTransaction] as tt
 			left join [dbo].[entity] en on (en.id=tt.[to_account])
-		WHERE tt.[date]< CONVERT(nvarchar(8),'@p1',112)  and en.type='Store'
-		and not tt.[to_product] is null
-		) ttt
-		${sw}
-		group by
+	WHERE tt.[date]< CONVERT(nvarchar(8),@p1,112)  and en.type='Store'
+	and not tt.[to_product] is null
+	) ttt
+	${sw}
+	group by
 			ttt.StoreID,
 			ttt.ProductID
-		having sum(ttt.Amount) <>0)
-		order by sz.StoreID`;
-	const response = await ssql.manyOrNone(newSQL, [dtSQLnormaliz]);
+	having sum(ttt.Amount) <>0) sz
+	order by sz.StoreID`;
+
+	// const response = await ssql.manyOrNone(newSQL, [dtSQLnormaliz]);
+	const storeUniqueResponce = {};
+
+
+	for (const { SKU2 } of responceLEFTROVERS) {
+		if (storeUniqueResponce[SKU2] !== undefined) {
+			return;
+		} else {
+			storeUniqueResponce[SKU2] = '';
+		}
+	}
+	for (const [storeGUID] of Object.entries(storeUniqueResponce)) {
+		let NOSQLDOC: any = {
+			Suplier: {},
+			Storehouse: {},
+			Department: {},
+			PayDate: Date,
+			Items: []
+		}
+		const storeInternal: any = await GetCatalog(
+			syncParams.project.id,
+			storeGUID,
+			syncParams.source.id,
+			'Storehouse',
+			dsql,
+		);
+		const company = (await dsql.oneOrNone<{ id: Ref }>(
+			// ! todo Nikita say:  я не уверен что это правильно?
+			`SELECT * from [Catalog.Company.v] where id=@p1`,
+			[storeInternal.company],
+		))!; // компания
+
+		const department = (await dsql.oneOrNone<{ id: Ref }>(
+			// ! todo Nikita say:  я не уверен что это правильно?
+			`SELECT * from [Catalog.Department.v] where id=@p1`,
+			[storeInternal.doc.Department],
+		))!; // департамент
+		const ResultDocOfBaseRecivier = (await dsql.oneOrNone<{ id: Ref }>(
+			// ! todo Nikita say:  я не уверен что это правильно?
+			` SELECT id FROM [Document.Operation.v] d where d.date=@p1 and d.Operation=@p2 and d.f2=@p3 `,
+			[syncParams.periodBegin, department.id, storeInternal.id],
+		))!; // проверка на сущестование документа в базе приемнике
+		if (ResultDocOfBaseRecivier) {
+
+		} else {
+			NOSQLDOC.Storehouse = storeInternal;
+			NOSQLDOC.Department = department;
+			NOSQLDOC.Suplier 
+			// todo ? https://smv-jetti.web.app/Catalog.Operation/4CF6EAC0-D2FD-11E9-A931-91F77FF9385E согласны этим данным у нас есть Supler
+			NOSQLDOC.PayDate = syncParams.periodBegin;
+			// ! todo Nikita say:  я не уверен что это правильно?
+			NOSQLDOC.Items = storeInternal;
+		}
+	}
 }
