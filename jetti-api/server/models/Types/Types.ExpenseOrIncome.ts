b6fb9c518425f039ef49554cc2b2ae0668dd140e
gimpon <dbez@outlook.com>
2020-11-09 09:46:27 +0200

    +TypesExpenseOrIncome

--- jetti-api/server/models/Types/Types.ExpenseOrIncome.ts
+++ jetti-api/server/models/Types/Types.ExpenseOrIncome.ts
@@ -1,10 +1,10 @@
 import { RegisteredDocument } from '../documents.factory';
 import { TypesBase } from './TypesBase';
 
-export class TypesExpenseOrBalanceOrIncome extends TypesBase {
+export class TypesExpenseOrIncome extends TypesBase {
 
   getTypes() {
-    const types = ['Catalog.Expense', 'Catalog.Balance', 'Catalog.Income'];
+    const types = ['Catalog.Expense', 'Catalog.Income'];
     return RegisteredDocument()
       .filter(d => types.includes(d.type))
       .map(e => e.type);
